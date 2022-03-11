2794939be0560e6ce708e98872a3b6b67588fef9
AveIvan94 <a.iwanowska@icloud.com>
2022-03-11 10:25:33 +0100

    add new code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[]args){
-        System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("twoje hobby");
+        String hobby = scanner.nextLine();
+        System.out.println("ulubiony kolor");
+        String kolor = scanner.nextLine();
+        System.out.println("ulubiona potrawa");
+        String potrawa = scanner.nextLine();
+
     }
 }
